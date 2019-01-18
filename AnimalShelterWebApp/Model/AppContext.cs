52f4c89a2fcdc4d6c33d2b41ddbbfbc6fbde345f
Karolina Makowska <karolina.makowska.kk@gmail.com>
2019-01-18 11:45:16 +0100

    Migrations in Progress

---
+++ AnimalShelterWebApp/Model/AppContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Model.Configuration;
+using Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<AboutShelterInfo> AboutShelterInfos { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Resident> Residents { get; set; }
+        public DbSet<Subscriber> Subscribers { get; set; }
+        public AppContext() : base("AppConnection")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new CategoryConfiguration());
+            modelBuilder.Configurations.Add(new OrderConfiguration());
+            modelBuilder.Configurations.Add(new ProductConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+        public static AppContext Create()
+        {
+            return new AppContext();
+        }
+
+    }
+}
